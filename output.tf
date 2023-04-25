30317f9ce6bf49e881d4a5bec09a84ce1ea17105
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2023-04-25 18:36:33 +0200

    update

--- output.tf
+++ output.tf
@@ -1,3 +1,19 @@
 output "network" {
   value = module.network
 }
+
+output "public_subnet_ids" {
+  value = module.network.public_subnet_ids
+}
+
+output "vpc_id" {
+  value = module.network.vpc_id
+}
+
+output "vpc_arn" {
+  value = module.network.vpc_arn
+}
+
+output "public_subnet_ids" {
+  value = module.network.public_subnet_ids
+}
